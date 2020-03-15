1d8506f96acb96f4fd58a70817e05044bd84603b
Ernesto Toncel <ernestotoncel@MacBook-Air-de-Ernesto.local>
2020-03-14 23:10:23 -0500

    navigation graph ands walkthrough fragments added

---
+++ app/src/main/java/com/example/sm4sh/ui/intro_walkthrough/WalkThroughFragmentThird.kt
@@ -0,0 +1,24 @@
+package com.example.sm4sh.ui.intro_walkthrough
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.sm4sh.R
+
+
+class WalkThroughFragmentThird : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_walkthrough_third, container, false)
+    }
+
+
+}
