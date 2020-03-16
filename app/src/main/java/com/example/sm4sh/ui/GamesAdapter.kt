88aa2f4b53dfeed56a9823b50e82133167e2e06a
Ernesto Toncel <ernestotoncel@MacBook-Air-de-Ernesto.local>
2020-03-15 23:33:06 -0500

    fetched game image

--- app/src/main/java/com/example/sm4sh/ui/GamesAdapter.kt
+++ app/src/main/java/com/example/sm4sh/ui/GamesAdapter.kt
@@ -4,17 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.bumptech.glide.Glide
 import com.example.sm4sh.R
 import com.example.sm4sh.model.Game
 import com.example.sm4sh.ui.utils.UIUtils
 import kotlin.math.roundToInt
 
 class GamesAdapter(val context: Context, val list: List<Game>) : Adapter<GamesAdapter.GameItemViewHolder>() {
-
-
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameItemViewHolder {
@@ -34,11 +34,14 @@ class GamesAdapter(val context: Context, val list: List<Game>) : Adapter<GamesAd
     override fun onBindViewHolder(holder: GameItemViewHolder, position: Int) {
         holder.gameNameTv.text = list[position].name
         holder.gameUniverseTv.text = list[position].universe
+        Glide.with(context).load(list[position].imageURL)
+            .into(holder.gameImage)
     }
 
 
     inner class GameItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        val gameImage:AppCompatImageView = itemView.findViewById(R.id.gameImageView)
         val gameNameTv:AppCompatTextView = itemView.findViewById(R.id.gameNameTextView)
         val gameUniverseTv:AppCompatTextView = itemView.findViewById(R.id.gameUniverseTextView)
 
